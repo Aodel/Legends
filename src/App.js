8517fd50207b14a7f8475500f95af5f21cae50e0
aoliverasd <aoliverasdelso@gmail.com>
2022-07-20 15:45:20 +0200

    first  commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <><div class="fw-bold">Bootstrap TRY! LEGENDS :D
+    </div><button type="button" class="btn btn-primary text-white bg-btn ">Detalles</button>
+    <div class="d">HOLA MUNDO!</div>
+    </>
+  );
+}
+
+export default App;
