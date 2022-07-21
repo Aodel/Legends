d949a6d2b9807cf9e2095f471b995f7699090923
Atcc627 <alangarciamartinez627@gmail.com>
2022-07-21 20:47:05 +0200

    links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,9 +13,10 @@ function Footer() {
       
      
         <div class="info">
-          <div class="contactos">Contacto</div>
-          <div class="contactos">Trabaja con nosotros</div>
-          <div class="contactos">Sobre nosotros</div>
+          <a href="https://github.com/aoliverasd/Legends"><div class="contactos">GitHub</div></a>
+          <div class="contactos">EQUIPO: Natalia-Cecilia-Cristal-Marcela-Nadia-Erick-Jerry-Abel-Alan</div>
+          {/* <div class="contactos">Trabaja con nosotros</div>
+          <div class="contactos">Sobre nosotros</div> */}
         </div>
      
       <div class="copy">Copyright 2022 S.L.</div>
