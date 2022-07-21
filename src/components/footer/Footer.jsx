7c683d5312ea5e89ef7173ed7d6797d03b910344
Atcc627 <alangarciamartinez627@gmail.com>
2022-07-21 14:50:51 +0200

    create css footer branch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import "../footer/footer.css"
+
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+
+
+   
+   
+    <div class="footer">
+      
+     
+        <div class="info">
+          <div class="contactos">Contacto</div>
+          <div class="contactos">Trabaja con nosotros</div>
+          <div class="contactos">Sobre nosotros</div>
+        </div>
+     
+      <div class="copy">Copyright 2022 S.L.</div>
+
+    </div>
+
+   
+
+
+    
   )
+
 }
 
 export default Footer
