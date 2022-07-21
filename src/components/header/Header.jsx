8a9da244ff191dd9a69e62ab5c1955161a60b5aa
unknown <nadialejandra87@gmail.com>
2022-07-21 14:39:54 +0200

    create header-branch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Header from "../header"
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="navbarcolor" expand="lg">
       <Container fluid>
         <Navbar.Brand href="#">LEGENDS</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -17,21 +17,9 @@ function Header() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action1">Regístrate</Nav.Link>
+            <Nav.Link href="#action2">Subir cómic</Nav.Link>
+            
           </Nav>
           <Form className="d-flex">
             <Form.Control
@@ -40,7 +28,7 @@ function Header() {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success">Búscalo por ISBN</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
