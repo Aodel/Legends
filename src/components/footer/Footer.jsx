bd5746c1ac6ce41db502b0c065dd63af18b5e1fd
Atcc627 <alangarciamartinez627@gmail.com>
2022-07-21 17:42:13 +0200

    vinculos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
+import "../footer/footer.css"
+
+
 
 function Footer() {
   return (
 
 
+   
+   
+    <div class="footer">
+      
+     
+        <div class="info">
+          <div class="contactos">Contacto</div>
+          <div class="contactos">Trabaja con nosotros</div>
+          <div class="contactos">Sobre nosotros</div>
+        </div>
+     
+      <div class="copy">Copyright 2022 S.L.</div>
+
+    </div>
+
+   
 
 
-    <div>Footer</div>
-  
-  
-  
-    )
+    
+  )
+
 }
 
 export default Footer
