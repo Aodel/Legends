d92cb7fd8c933fe11634786a4f1c81e4d25c0d8e
aoliverasd <aoliverasdelso@gmail.com>
2022-07-22 03:27:05 +0200

    searchbar on progress

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,9 +5,47 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Stack from 'react-bootstrap/Stack';
 import logoBuho from '../../assets/img/logoBuho.png'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function Header() {
+
+
+  const apiUrl =
+    "https://gateway.marvel.com:443/v1/public/comics?ts=3000&apikey=c8ddc405fae663f25d5aae45bac426cc&hash=648ac7a873f5e371d1e7929a23ade05d";
+
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then(res => {
+        setComics(res.data.data.results);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+
+
+  const [search, setSearch]= useState("");
+  //SearchBar
+
+  const saveSearch= e=>{
+    setSearch(e.target.value);
+    filter(e.target.value);
+  }
+
+  const filter=(searchTerm)=>{
+    let searchResult= comics.filter((element)=>{
+      if(element.result.title.toString().toLowerCase().includes(searchTerm.toLowerCase())){
+        return element;
+      }
+
+    });
+    setComics(searchResult);
+  }
+
   return (
     <Navbar bg="danger" expand="lg">
       <Container fluid>
@@ -35,6 +73,7 @@ function Header() {
               type="search"
               placeholder="Encuentra tu cómic"
               className="me-3"
+              onChange={saveSearch}
               
             />
             <Button variant="warning"><i class="fa-solid fa-magnifying-glass"></i></Button>{' '}
