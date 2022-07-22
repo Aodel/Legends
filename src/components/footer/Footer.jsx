b7117e484d1ad38b774361da8430bb6145e65044
aoliverasd <aoliverasdelso@gmail.com>
2022-07-22 02:10:16 +0200

    footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import "../footer/footer.css"
+import Image from 'react-bootstrap/Image'
+import github from '../../assets/img/github.svg'
 
 
 
 function Footer() {
   return (   
    
-    <div className="footer">
+    <div className="footer justify-content-center">
       
      
-        <div className="info">
-          <a href="https://github.com/aoliverasd/Legends"><div class="contactos">GitHub</div></a>
+        <div className="info align-items-center">
+          <a href="https://github.com/aoliverasd/Legends"><Image src={github} style={{ height:'3rem'}} /></a>
+          
           <div className="contactos">Legend Crew: Natalia-Cecilia-Cristal-Marcela-Nadia-Erick-Jerry-Abel-Alan</div>
           {/* <div class="contactos">Trabaja con nosotros</div>
           <div class="contactos">Sobre nosotros</div> */}
         </div>
      
-      <div className="copy">Copyright 2022 S.L.</div>
+      <div className="copy">Copyright LEGENDS 2022 S.L.</div>
 
     </div>
 
